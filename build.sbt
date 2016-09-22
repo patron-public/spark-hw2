94338031246521eb2540feaa175fafa5b0a83503
pavel <pavel.yakimchyk@gmail.com>
2016-09-22 11:43:08 +0300

    df logic

--- build.sbt
+++ build.sbt
@@ -8,6 +8,8 @@ mainClass in Compile := Some("com.epam.spark.training.CsvFlightsProcessor")
 
 libraryDependencies += "org.apache.spark" % "spark-sql_2.10" % "1.6.0"
 
+libraryDependencies += "com.databricks" % "spark-csv_2.10" % "1.5.0"
+
 libraryDependencies ++= Seq(
   ("org.apache.spark" % "spark-core_2.10" % "1.6.0").
     exclude("org.mortbay.jetty", "servlet-api").
--- src/main/scala-2.10/com/epam/spark/training/dataframes/CsvFlightsProcessor.scala
+++ src/main/scala-2.10/com/epam/spark/training/dataframes/CsvFlightsProcessor.scala
@@ -1,26 +1,35 @@
 package com.epam.spark.training.dataframes
 
-import com.epam.spark.training.dataframes.model.{Airport, Carrier, Flight}
+import com.epam.spark.training.dataframes.model.{Carrier, Flight}
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
+
+
+case class Airport(id: String, name: String, city: String, state: String, country: String, lat: Double, long: Double)
+
 object CsvFlightsProcessor {
+
   val appName:String = "Server Log Analyzer"
   val master:String = "local"
   var sc:SparkContext = _
-  var sqlContext = _
+  var airports:DataFrame = _
+  var carriers:DataFrame = _
+  var flights:DataFrame = _
 
-  def readFiles() = {
 
-    val airports = sc.textFile("/training/spark/hw2/data/airports.csv")
+  def readFiles(sqlContext: SQLContext) = {
+    import sqlContext.implicits._
+    airports = sc.textFile("/training/spark/hw2/data/airports.csv")
       .filter(header => !header.startsWith(""""iata""""))
       .map(_.split("(?:^\")|(?:(\")?[,](?=[\\d-]))|(?:\",\")"))
       .filter(arr => arr.length==8) //NA= not USA
       .map(p => Airport(p(1), p(2), p(3), p(4), p(5), p(6).toDouble, p(7).toDouble))
       .toDF()
 
-    airports.registerTempTable("airports")
+      airports.registerTempTable("airports")
 
-    val carriers = sc.textFile("/training/spark/hw2/data/carriers.csv")
+    carriers = sc.textFile("/training/spark/hw2/data/carriers.csv")
       .filter(header => !header.equals("Code,Description"))
       .map(_.split("(?:^\")|(?:[\"][,][\"])|(?:\"$)"))
       .map(p => Carrier(p(1), p(2)))
@@ -32,13 +41,20 @@ object CsvFlightsProcessor {
       .filter(hdr => !hdr.startsWith("Year"))
       .map(_.split(","))
 
-    val flights = flightsTextArrRDD
+    flights = flightsTextArrRDD
       .map(p => Flight(p(8), p(16), p(17), p(21).equals("1")))
       .toDF()
 
     flights.registerTempTable("flights")
 
   }
+
+  def getSqlContext():(SQLContext) = {
+    val conf = new SparkConf().setAppName(appName).setMaster(master)
+    sc = new SparkContext(conf)
+    return new SQLContext(sc)
+  }
+
 
   def main(args: Array[String]) {
 
@@ -47,17 +63,14 @@ object CsvFlightsProcessor {
       System.exit(1)
     }
 
-    val conf = new SparkConf().setAppName(appName).setMaster(master)
-    sc = new SparkContext(conf)
-    sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val sqlContext = getSqlContext()
 
-    readFiles()
-//    sqlContext.read.text("/tarining/spark/hw2/data/airports.csv")
-    val dataSet = sc.textFile(args(0))
+    readFiles(sqlContext)
 
-
-
-    dataSet.saveAsTextFile("hdfs://"+ args(1))
+//    flights.groupBy("carrier_id").count.sort($"count".desc).limit(5).show()
+//    flights.filter(!(flights("cancelled"))).groupBy("carrier_id").count.sort($"count".desc).limit(5).show
+    flights.groupBy("carrier_id").count.show()
+    flights.filter(!(flights("cancelled"))).groupBy("carrier_id").count.show
   }
 
 
